a3336157cd87c5c66ab6dee5c281bd7b0f709c89
James Chong <wei3hua2@gmail.com>
2018-08-02 22:40:03 +0800

    change keyword

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,37 @@
 let Hogan = require('hogan.js');
 import {RpsContext,RpsModule,rpsAction} from 'rpscript-interface';
 
-/** Hogan Module
+/** Module for Hogan templating
+ * @see {@link http://twitter.github.io/hogan.js/|Hogan}
  * @namespace Hogan
+ * @example
+ * rps install hogan
 */
 @RpsModule("hogan")
 export default class RPSHogan {
 
-  @rpsAction({verbName:'compile-hogan'})
-  async moustacheToHtml (ctx:RpsContext,opts:Object, content:string, data?:Object) : Promise<string|Function>{
+  /**
+ * @function hogan-compile
+ * @memberof Hogan
+ * 
+ * hogan-compile '<template-content' {a:1,b:2}
+ * 
+ * @param {String} template template content.
+ * @param {Object} context context to be processed.
+ * @param {*} options List of options provided.
+ * 
+ * @returns {void}
+ * @summary hogan-compile :: String → String
+ * 
+*/
+  @rpsAction({verbName:'hogan-compile'})
+  async moustacheToHtml (ctx:RpsContext,opts:Object, content:string, context?:Object) : Promise<string|Function>{
     if(!content) throw Error('Hogan content not found');
     else {
       let compiled = Hogan.compile(content,opts);
 
-      if(!data) return compiled;
-      else return compiled.render(data);
+      if(!context) return compiled;
+      else return compiled.render(context);
     }
   }
 
